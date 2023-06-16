3b3c847956d97379a5861015df30f3e9bdab9d7c
Jamal Gibson <jamal.gibson@rogers.com>
2023-06-16 01:03:35 -0400

    updated portfolio

--- js/blogApi.js
+++ js/blogApi.js
@@ -1,5 +1,5 @@
 function fetchBlogData() {
-    const baseUrl = "https://crucibleblogmvc-production.up.railway.app";
+    const baseUrl = "https://bullog.up.railway.app";
 
     fetch(`${baseUrl}/api/BlogPosts/portfolio/3`)
       .then((response) => response.json())
